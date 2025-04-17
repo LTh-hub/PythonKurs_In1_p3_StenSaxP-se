75f489b4aef0cd835a460cb74d07285eafb78bd2
Lars Thomsson <lartho63@gmail.com>
2025-04-17 14:51:05 +0200

    Sten-Sax-Påse, Inlämningsuppgift 1/proj 3

---
+++ ssp.py
@@ -0,0 +1,44 @@
+# ssp.py
+#
+#   Pythonproframmering för AI-utveckling
+#
+#       Inmlämningsuppgift 1
+#
+#   Projekt 3: Sten-sax-påse
+#       Skapa en version av spelet sten-sax-påse.
+#       Datorn slumpar vilken av sten, sax eller påse den ska välja.
+#       Spelaren väljer också sten, sax eller påse.
+#       Datorn och spelaren visar sedan upp sina val samtidigt.
+#
+#       Reglerna är enligt följande:
+#           - sten vinner över sax
+#           - sax vinner över påse 
+#           - påse vinner över sten
+#           - oavgjort om båda väljer samma alternativ
+#
+#   Spelaren spelar tills hen vinner eller förlorar mot datorn.
+#
+#
+import tkinter as tk
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root.mainloop()
+
+
+
+
+
+
+
+
+# git status (frivilligt)
+# git add .
+# git commit  -m"Added new code for mongo"
+# git push
+
